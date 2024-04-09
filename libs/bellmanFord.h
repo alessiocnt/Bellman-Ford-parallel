c6cda3cb21e5ff00b89f70729795ad13d8115e02
Alessio Conti <alessio.conti@cervia.com>
2024-04-09 12:40:30 +0200

    parallel version of bellman ford

--- libs/bellmanFord.h
+++ libs/bellmanFord.h
@@ -3,4 +3,5 @@
 	#include "graph.h"
     // Bellman Ford algorithm, returns 0 if a solution is given, -1 if there is a negative cycle
 	int bellmanFord(struct Graph* graph, int src);
+	int bellmanFord_parallel(struct Graph* graph, int src);
 #endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "bellmanFord.h"
 #include "graph.h"
 #include "utils.h"
+#include <omp.h>
 
 int main()
 {
-	struct Graph* graph = importGraphFromFile("../benchmark/graph_M_3.txt");
-	bellmanFord(graph, 0);
+	struct Graph* graph = importGraphFromFile("./benchmark/graph_003_M.txt");
+	omp_set_num_threads(4);
+	bellmanFord_parallel(graph, 0);
 	return 0;
 }
