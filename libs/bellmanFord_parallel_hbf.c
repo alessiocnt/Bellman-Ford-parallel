f2bb478898036eea648d09de8938af42f8da2622
Alessio Conti <alessio.conti@cervia.com>
2024-04-11 00:24:36 +0200

    frontier based BF

--- libs/bellmanFord_parallel_hbf.c
+++ libs/bellmanFord_parallel_hbf.c
@@ -18,6 +18,29 @@ int bellmanFord_parallel(struct Graph* graph, int src) {
     dist[src] = 0;
 
     struct Frontier* f1 = createFrontier(V); // Set capacity to the max number of vertex
+    enqueue(f1, struct Edge node);
+    struct Frontier* f2 = createFrontier(V); // Empty
+
+    // currentIteration = 0 -> see paper
+    while !isEmpty(f1) { // or frontiera > V per cicli negativi
+        #pragma omp parallel for
+        for(int i=0; i<getLength(f1); i++) {
+            struct Node node = dequeue(f1);
+            int u = node.value;
+            #pragma omp parallel for
+            for (int j=0; j<node.outEdgesSize; j++) {
+                struct Node dest = node.outEdges[j].dest;
+                int v = dest.value;
+                int weight = node.outEdges[j].weight;
+                if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+                    #pragma omp critical
+                    dist[v] = dist[u] + weight;
+                    enqueue(f2, dest);
+                }
+            }
+        }
+        swap(&f1, &f2);
+    }
 
     // Relax all edges |V|-1 times
     for (int i=1; i<=V-1; i++) {
