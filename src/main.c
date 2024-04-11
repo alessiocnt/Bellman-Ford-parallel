6e843a5e6aff14932e0360e17fb2ece2e1652ddc
Alessio Conti <alessio.conti@cervia.com>
2024-04-11 18:03:17 +0200

    main for testing time

--- src/main.c
+++ src/main.c
@@ -1,12 +1,32 @@
+/****************************************************************************
+*
+* Run with:
+* ./bellman_ford n_threads
+*
+****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 #include "graph.h"
 #include "bellmanFord.h"
 #include "utils.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-	struct Graph* graph = importGraphFromFile("./benchmark/graph_001_XS.txt");
-	omp_set_num_threads(4);
+	float tstart, elapsed;
+	if (argc == 2) {
+		int threadNum = atoi(argv[1]);
+		omp_set_num_threads(threadNum);
+    } 
+	struct Graph* graph = importGraphFromFile("./benchmark/graph_003_M.txt");
+	
+	tstart = omp_get_wtime();
 	bellmanFord_parallel_hbf(graph, 0);
-	return 0;
+	elapsed = omp_get_wtime() - tstart;  
+
+	printf("Executed concurrently with %d threads \n", omp_get_max_threads());
+    printf("Elapsed time %f\n", elapsed);
+    return EXIT_SUCCESS;
+	// return 0;
 }
