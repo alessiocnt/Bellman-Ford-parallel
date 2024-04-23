425d5353797f0c0edca8d5cd21ebef28df3e7400
Alessio Conti <alessio.conti@cervia.com>
2024-04-23 14:56:38 +0200

    algorithm update

--- libs/utils.h
+++ libs/utils.h
@@ -1,8 +1,8 @@
 #ifndef UTILS_H
 	#define UTILS_H
-
     # define INIT 0     // INT_MAX
-    
+    double gettime(void);
     struct Graph *importGraphFromFile(const char *filename);
-    void printGraphToFile(int **matrix, int V, int E, const char *filename);
+    void freeDevice(struct Graph* d_graph, int* d_dist, int* d_isNegativeCycle);
+    void freeHost(struct Graph* h_graph, int* h_dist)
 #endif
