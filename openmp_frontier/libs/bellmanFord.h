82d8b4a5e62f78e85fcf8f0f897ba01b8c2215cb
Alessio Conti <alessio.conti@cervia.com>
2024-05-07 17:17:27 +0200

    resolved merge conflicts

--- openmp_frontier/libs/bellmanFord.h
+++ openmp_frontier/libs/bellmanFord.h
@@ -2,10 +2,5 @@
 	#define BELLMANFORD_H
 	#include "graph.h"
     // Bellman Ford algorithm, returns 0 if a solution is given, -1 if there is a negative cycle
-<<<<<<<< HEAD:openmp_frontier/libs/bellmanFord.h
 	int bellmanFord_parallel_hbf(struct Graph* graph, int src);
-========
-	int bellmanFord(struct Graph* graph, int src);
-	int bellmanFord_parallel(struct Graph* graph, int src);
->>>>>>>> develop:openmp_base/libs/bellmanFord.h
 #endif
