3147ebe6ee404b4ba372a3cd21609f4f3da6b82e
Alessio Conti <alessio.conti@cervia.com>
2024-05-07 10:43:37 +0200

    removed libs and modified main

--- libs/bellmanFord.h
+++
@@ -1,6 +0,0 @@
-#ifndef BELLMANFORD_H
-	#define BELLMANFORD_H
-	#include "graph.h"
-    __global__ void bellmanFord(Graph *d_graph, int *d_dist);
-	__global__ void checkNegativeWeightCycles(Graph *d_graph, int *d_dist, int *d_isNegativeCycle);
-#endif
